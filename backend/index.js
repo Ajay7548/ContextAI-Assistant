89be0204e5662e0d839ecea65c8e772483213c69
Ajay Mourya <mouryaajay7548@gmail.com>
2026-04-07 23:58:32 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,9 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:process.env.FRONTEND_URL || "http://localhost:5173"
+}))
 
 app.get('/',(req,res)=>{
     res.send('ContextAI Backend Running')
